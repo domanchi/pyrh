542f197af0110dc754459bf2f43ae5d3cf164153
Aaron Loo <admin@aaronloo.com>
2020-12-10 08:51:34 -0800

    adding setup.py for pip compatibility

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='pyrh',
+    packages=find_packages(exclude=(['test*', 'tmp*'])),
+    version='2.0',
+    license='MIT',
+    instalL_requires=[
+        'marshmallow',
+        'python-dateutil',
+        'pytz',
+        'requests',
+        'yarl',
+        'certifi',
+    ],
+)
